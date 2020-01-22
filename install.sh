4e633b44efb54a185a62a5c8f459ec0987fdad7b
Brent Hopkins <52512160+brent901@users.noreply.github.com>
2020-01-22 11:25:33 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+echo "WSE must be installed by itself for compatibility and security reasons. It also works best on a fresh installation of Ubuntu. Type 'y' and enter to continue"
+read confirm
+if [ $confirm != "y" ]
+then
+echo "You need to enter 'y' in order to continue"
+exit
+fi
 apt update
-apt upgrade
+apt upgrade -y
 apt install net-tools
